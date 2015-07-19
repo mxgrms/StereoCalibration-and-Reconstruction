3c6899d1bb11b662397316650f63daf6b0fc101c
Maximilian Grams <maximiliangrams@Maximilians-MacBook-Pro.local>
2015-07-19 23:14:05 +0200

    V1.03
    - Formatted comments to fit the textwidth
    - Added explanation for finding out the disparity range that is used
    for calculating the disparity map
    - Changed the disparity range to fit the maximum image shift of -77
    pixels

--- StereoCalibration_Final.m
+++ StereoCalibration_Final.m
@@ -1,9 +1,10 @@
 %% Stereo Calibration, rectification, disparity map and 3D point cloud
 % Algorithm for perfoming a calibration of a stereo camera system.
-% By the use of the estimated camera parameters one can rectify the stereo
-% images in order to calculate the disparity map of each image pair.
-% Finally a 3D point cloud reconstructs the scene to determine the distance
-% of object points.
+% By the use of the estimated camera parameters one can rectify the ...
+% stereo images in order to calculate the disparity map of each image ...
+% pair.
+% Finally a 3D point cloud reconstructs the scene to determine the ...
+% distance of object points.
 
 %% Stereo Calibration
 
@@ -18,17 +19,21 @@ imageDir = fullfile(workspaceDir, 'StereoCalibration', 'Grossbild');
 
 % Filenames
 for i = 1:numImagePairs
-    imageFileNames1{i} = fullfile(imageDir, 'Left',  sprintf('L%d.jpeg', i));
-    imageFileNames2{i} = fullfile(imageDir, 'Right', sprintf('R%d.jpeg', i));
+    imageFileNames1{i} = fullfile(imageDir, 'Left', ...
+        sprintf('L%d.jpeg', i));
+    imageFileNames2{i} = fullfile(imageDir, 'Right', ...
+        sprintf('R%d.jpeg', i));
 end
 
 % Detect checkerboards in images
-[imagePoints, boardSize, imagesUsed] = detectCheckerboardPoints(imageFileNames1, imageFileNames2);
+[imagePoints, boardSize, imagesUsed] = ...
+    detectCheckerboardPoints(imageFileNames1, imageFileNames2);
 
 % Number of picture(s) without detected checkerboard points
 notUsed = find(ismember(imagesUsed, 0, 'rows'));
 if numImagePairs ~= notUsed
-hint = sprintf('\nWarning: Failed to detect checkerboard points in stereo pair %d!\n', notUsed);
+hint = sprintf(['\nWarning: Failed to detect checkerboard points in' ...
+    ' stereo pair %d!\n'], notUsed);
 disp(hint);
 end
 
@@ -37,7 +42,8 @@ squareSize = 25;  % in units of 'mm'
 worldPoints = generateCheckerboardPoints(boardSize, squareSize);
 
 % Calibrate the camera
-[stereoParams, pairsUsed, estimationErrors] = estimateCameraParameters(imagePoints, worldPoints, ...
+[stereoParams, pairsUsed, estimationErrors] = ...
+    estimateCameraParameters(imagePoints, worldPoints, ...
     'EstimateSkew', false, 'EstimateTangentialDistortion', false, ...
     'NumRadialDistortionCoefficients', 2, 'WorldUnits', 'mm');
 
@@ -66,18 +72,30 @@ I2{i} = imread(imageFileNames2{i});
 [J1{i}, J2{i}] = rectifyStereoImages(I1{i}, I2{i}, stereoParams);
 end
 
+% Specify 'DisparityRange' of the disparity function by measuring the ...
+% horizontal shift
+% -> Has to be specified for every distance of the focal plain due to ...
+% the dependence of the disparity value from the object distance
+
+% A = stereoAnaglyph(J1{1}, J2{1});
+% imtool(A);
+
+% Maximum shift from left to right image: -77 Pixel in image 5
+% -> DisparityRange = [-80, 0] (must be dividible by 16!)
+
 for i = 1:size(pairsUsed,1)
 %Display undistorted image
 figure('Name', 'Comparison of rectified image and disparity map', ...
-    'NumberTitle', 'off', 'Units', 'normalized', 'Position', [0, 0, 1, 0.8]);
+    'NumberTitle', 'off', 'Units', 'normalized', 'Position', ...
+    [0, 0, 1, 0.8]);
 subplot(1,2,1); imshow(J1{i}); title('Rectified Image', 'FontUnits', ...
     'points', 'FontSize', 15);
 
 % Display disparity map
 disparityMap = disparity(rgb2gray(J1{i}), rgb2gray(J2{i}), ...
-    'DisparityRange', [-96 0], 'Method','SemiGlobal', ...
+    'DisparityRange', [-80 0], 'Method','SemiGlobal', ...
     'UniquenessThreshold', 5, 'DistanceThreshold', 40);
-subplot(1,2,2); imshow(disparityMap, [-96 0], ...
+subplot(1,2,2); imshow(disparityMap, [-80 0], ...
     'InitialMagnification', 50); title('Disparity Map', 'FontUnits', ...
     'points', 'FontSize', 15);
 colormap('jet');
@@ -109,15 +127,15 @@ while ~rightNumber
     if (imageNumber >= 1) && (imageNumber <= numImagePairs)
         rightNumber = true;
     else
-        warning = sprintf('\nWarning: You can only choose a picture between %d and %d\n',...
-        1, numImagePairs);
+        warning = sprintf(['\nWarning: You can only choose a picture' ...
+            ' between %d and %d'], 1, numImagePairs);
         disp(warning);
     end
 end
 
-disparityMap = disparity(rgb2gray(J1{imageNumber}), rgb2gray(J2{imageNumber}), ...
-    'DisparityRange', [-96 0], 'Method','SemiGlobal', ...
-    'UniquenessThreshold', 5, 'DistanceThreshold', 40);
+disparityMap = disparity(rgb2gray(J1{imageNumber}), ...
+    rgb2gray(J2{imageNumber}), 'DisparityRange', [-80 0], 'Method', ...
+    'SemiGlobal', 'UniquenessThreshold', 5, 'DistanceThreshold', 40);
 
 % Reconstruction of disparity map
 point3D = reconstructScene(disparityMap, stereoParams);
@@ -130,11 +148,13 @@ z = point3D(:, :, 3);
 minZ = 0; % Minimum distance in meters
 maxZ = 2; % Maximimum distance in meters
 zdisp = z;
-zdisp(z < minZ | z > maxZ) = NaN; % Erase all points outside the distance interval
+% Erase all points outside the distance interval
+zdisp(z < minZ | z > maxZ) = NaN; 
 point3Ddisp = point3D;
 point3Ddisp(:,:,3) = zdisp; % Write back new distance values
 figure('Name', 'Reconstructed scene from disparity map', ...
-    'NumberTitle', 'off', 'Units', 'normalized', 'Position', [0, 0, 1, 0.8]); ...
+    'NumberTitle', 'off', 'Units', 'normalized', 'Position', ...
+    [0, 0, 1, 0.8]);
 showPointCloud(point3Ddisp, J1{imageNumber}, 'VerticalAxis', 'Y',...
     'VerticalAxisDir', 'Down' );
 xlabel('X');
